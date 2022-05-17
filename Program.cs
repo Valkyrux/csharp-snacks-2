3395bb57d2be19ae8d31af09f393588f30466da8
Phil.adelphos <phil.adelphos.dev@gmail.com>
2022-05-17 18:12:04 +0200

    complete

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-List<int> listInt = new List<int>() {1, 2, 3, 4, 5, 6, 7};
+﻿List<int> listInt = new List<int>() {1, 2, 3, 4, 5, 6, 7};
 
 var mettiAlQuadrato1 = () => { int tot = 0;  return ((int i) => { tot += i; return tot; }); };
 var ciao = mettiAlQuadrato1();
+
+Console.WriteLine("Accumulatore con metodo CLOSURE");
 Console.WriteLine(ciao(10));
+Console.WriteLine(ciao(20));
 Console.WriteLine(ciao(20));
 
 List<int> listAlQuadrato = mettiAlQuadrato(listInt);
@@ -31,8 +31,8 @@ List<int> mettiAlCubo(List<int> listInt)
     }
     return listAlQuadrato;
 }
-
-Console.WriteLine("\nNUMERI:");
+Console.WriteLine("____________________________");
+Console.WriteLine("NUMERI:");
 foreach (int i in listInt)
 {
     Console.Write("{0}, ", i);
@@ -65,8 +65,113 @@ List<int> EseguiCalcolo(List<int> li, Func<int, int> fun)
 
 List<int> listaAlQuadrato1 = EseguiCalcolo(listInt, (n) => n * n * n);
 List<int> listaDiSomme = EseguiCalcolo(listInt, (n) => n * (n + 1) / 2);
-
+Console.WriteLine("\n____________________________\n");
+Console.WriteLine("LISTA GENERATA DA FUNZIONE CON METODO CLOSURE:");
 foreach (int i in listaDiSomme)
 {
     Console.WriteLine(i);
+}
+
+//trovare da una lista di stringhe di partenza una lista di lunghezze rispettive ad ogni stringa
+List<string> listaDiStringhe = new List<string>() { "ciao", "come", "stai", "pieralberto angeli"};
+
+List<int> EseguiCalcolo1(List<string> listaStringhe, Func<string, int> fun)
+{
+    List<int> result = new List<int>();
+    foreach (string str in listaStringhe)
+    {
+        result.Add(fun(str));
+    }
+    return result;
+}
+
+
+List<int> listaLunghezze = EseguiCalcolo1(listaDiStringhe, (str) => str.Length);
+for (int i = 0; i < listaLunghezze.Count; i++)
+{
+    Console.WriteLine("{0} : {1}", listaDiStringhe[i], listaLunghezze[i]);
+}
+
+Dictionary<string, int> dizionario = new Dictionary<string, int>() { { "uno", 1 }, { "due", 2 }, { "tre", 3 }, { "quattro", 4 }, { "cinque", 5 }, { "sei", 6 } };
+
+List<string> listString = new List<string>() { "tre", "due", "uno", "Cinque", "cinque", "Tre" };
+Console.WriteLine("\n___LISTA STRINGHE INIZIALE___");
+foreach (string str in listString)
+{
+    Console.WriteLine(str);
+}
+
+
+listString.Sort(
+    (string s1, string s2) =>
+    {
+        if(dizionario[s1.ToLower()] > dizionario[s2.ToLower()])
+        {
+            return 1;
+        } 
+        else if(dizionario[s1.ToLower()] < dizionario[s2.ToLower()])
+        {
+            return -1;
+        }
+        else
+        {
+            return 0;
+        }
+    });
+
+Console.WriteLine("\n___ORDINAMENTO DEI NUMERI SECONDO IL SIGNIFICATO DELLA PAROLA___");
+foreach (string str in listString)
+{
+    Console.WriteLine(str);
+}
+
+listString.Sort();
+Console.WriteLine("\n___ORDINAMENTO LESSICALE___");
+foreach (string str in listString)
+{
+    Console.WriteLine(str);
+}
+
+listString.Sort((string s1, string s2) => -s1.CompareTo(s2));
+Console.WriteLine("\n___ORDINAMENTO LESSICALE DECRESCENTE___");
+foreach (string str in listString)
+{
+    Console.WriteLine(str);
+}
+
+List<Tuple<DateTime, int>> ListaFinale = new List<Tuple<DateTime, int>>();
+for (int i = 0; i<10; i++)
+{
+    DateTime start = new DateTime(1970, 1, 1);
+    int range = (DateTime.Today - start).Days;
+    DateTime randomDate = start.AddDays(new Random().Next(range));
+    ListaFinale.Add(new Tuple<DateTime, int>(randomDate, i));
+}
+
+Console.WriteLine("\n___TUPLE GENERATE___");
+foreach (var tuple in ListaFinale)
+{
+    Console.WriteLine("{0} - {1}/{2}/{3}", tuple.Item2, tuple.Item1.Day, tuple.Item1.Month, tuple.Item1.Year);
+}
+
+ListaFinale.Sort((t1, t2) =>
+{
+    if (t1.Item1 > t2.Item1)
+    {
+        return 1;
+    }
+    else if (t1.Item1 < t2.Item1)
+    {
+        return -1;
+    }
+    else
+    {
+        return 0;
+    }
+});
+
+Console.WriteLine("\n___TUPLE ORDINATE PER DATA___");
+foreach (var tuple in ListaFinale)
+{
+    Console.WriteLine("{0} - {1}/{2}/{3}", tuple.Item2, tuple.Item1.Day, tuple.Item1.Month, tuple.Item1.Year);
 }
